9a92ec235cc660bea8ad8ab06f46f8809a845b1a
mlodyasian <100943556+mlodyasian@users.noreply.github.com>
2022-03-11 14:49:28 +0100

    Add files via upload

---
+++ zgadnijliczbe.cs
@@ -0,0 +1,35 @@
+﻿Random r = new Random();
+
+int gowno = r.Next(1, 10);
+int zgaduj = 0;
+bool poprawny = false;
+
+Console.WriteLine("mysle o cyfrze od 1 do 10");
+
+while (!poprawny)
+ {
+    Console.Write("zgadnij numer: ");
+    string input = Console.ReadLine();
+
+    if (!int.TryParse(input, out zgaduj))
+    {
+        Console.WriteLine("to nie cyfra glabie");
+        continue;
+    }
+
+    if (zgaduj < gowno)
+    {
+        Console.WriteLine("wyzsza");
+    }
+    else if (zgaduj > gowno)
+    {
+        Console.WriteLine("nizsza");
+    }
+    else
+    {
+        poprawny = false;
+        Console.WriteLine("udalo sie:D");
+    }
+
+
+ }
